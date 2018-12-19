83b2924ed3c9f1b2c60d9d3639af0610366ee363
zlding <zlding@192.168.1.5>
2018-12-19 22:13:06 +0800

    init

---
+++ dails-parent/dails-plugins-example/src/main/java/org/dails/plugins/example/App.java
@@ -0,0 +1,13 @@
+package org.dails.plugins.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
